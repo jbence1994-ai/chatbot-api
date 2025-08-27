f5209dac30e6d170bf05b3fc1223b49cece615cb
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-08-27 11:48:10 +0200

    Rename main class

--- src/main/java/com/github/jbence1994ai/chatbot/api/Application.java
+++ src/main/java/com/github/jbence1994ai/chatbot/api/Application.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatbotApiApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChatbotApiApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
-
 }
