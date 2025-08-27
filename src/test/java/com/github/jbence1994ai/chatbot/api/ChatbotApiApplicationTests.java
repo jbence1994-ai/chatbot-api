d5110cc10638753c628c8d6a9f5f30efa09cbcd3
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-08-27 11:43:43 +0200

    Initial commit

---
+++ src/test/java/com/github/jbence1994ai/chatbot/api/ChatbotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jbence1994ai.chatbot.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
