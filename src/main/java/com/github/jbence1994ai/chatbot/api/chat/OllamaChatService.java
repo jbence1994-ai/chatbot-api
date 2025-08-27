ab2bffa7fcd8b0e25b2cc87c20bddabd50fde4a9
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-08-27 18:46:29 +0200

    Configure system prompt, context window size and max output tokens

--- src/main/java/com/github/jbence1994ai/chatbot/api/chat/OllamaChatService.java
+++ src/main/java/com/github/jbence1994ai/chatbot/api/chat/OllamaChatService.java
@@ -2,19 +2,32 @@ package com.github.jbence1994ai.chatbot.api.chat;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class OllamaChatService implements ChatService {
+    private final SystemPromptConfig systemPromptConfig;
     private final OllamaChatModel ollamaChatModel;
 
     @Override
-    public Chat chat(String prompt) {
+    public Chat chat(String message) {
         try {
-            var response = ollamaChatModel.call(prompt);
+            var systemMessage = new SystemMessage(systemPromptConfig.systemPrompt());
+            var userMessage = new UserMessage(message);
+            var prompt = new Prompt(List.of(systemMessage, userMessage));
+
+            var response = ollamaChatModel.call(prompt)
+                    .getResult()
+                    .getOutput()
+                    .getText();
 
             return new Chat(response);
         } catch (Exception exception) {
