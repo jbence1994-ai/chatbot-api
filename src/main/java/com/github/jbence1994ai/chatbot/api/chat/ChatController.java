88f03387dc7cf0ac4408a9f1b4bd8a38cee2f152
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-08-28 13:31:25 +0200

    Add stream version of chat

--- src/main/java/com/github/jbence1994ai/chatbot/api/chat/ChatController.java
+++ src/main/java/com/github/jbence1994ai/chatbot/api/chat/ChatController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/api/chat")
@@ -22,6 +23,17 @@ public class ChatController {
         log.info("ChatRequest with conversation ID: {}", request.conversationId());
 
         var chatResponse = chatService.chat(request);
+
+        log.info("ChatResponse for conversation ID: {}", request.conversationId());
+
+        return chatResponse;
+    }
+
+    @PostMapping("/stream")
+    public Flux<ChatResponse> chatStream(@Valid @RequestBody ChatRequest request) {
+        log.info("ChatRequest with conversation ID: {}", request.conversationId());
+
+        var chatResponse = chatService.chatStream(request);
 
         log.info("ChatResponse for conversation ID: {}", request.conversationId());
 
