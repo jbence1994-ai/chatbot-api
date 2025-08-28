ad79039d594d895ea8d22eabed79e716a6456286
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-08-28 15:20:54 +0200

    Remove all Flux code

--- src/main/java/com/github/jbence1994ai/chatbot/api/chat/ChatService.java
+++ src/main/java/com/github/jbence1994ai/chatbot/api/chat/ChatService.java
@@ -1,9 +1,5 @@
 package com.github.jbence1994ai.chatbot.api.chat;
 
-import reactor.core.publisher.Flux;
-
 public interface ChatService {
     ChatResponse chat(ChatRequest request);
-
-    Flux<ChatResponse> chatStream(ChatRequest request);
 }
